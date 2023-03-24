7fb65c80454744ba167b95c6cda85b9b3fa5eb1d
ORTIZ <ORTIZ@192.168.211.113>
2023-03-23 18:42:29 -0600

    carga de usuarios

---
+++ src/Controladores/PacientesController.java
@@ -0,0 +1,20 @@
+
+package Controladores;
+import Vistas.frmPrincipal;
+import java.avtevent.ActionEvent;
+
+
+
+public class PacientesController {
+frmPrincipal VistaPrincipal;
+PacientesModel ModeloPacientes;
+
+public PacientesController (frmPrincipal VistaPrincipal,frmPacientesModel ModeloPacientes){
+    this.VistaPrincipal.btnIngresar.addActionListener
+    
+    
+}
+
+    
+    
+}
